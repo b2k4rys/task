3c730e117eea7565864afc46d1e4d74861dd43d0
Bekarys <bekarys20050812@gmail.com>
2025-05-23 14:49:58 +0500

    add registration

--- olx/main/views.py
+++ olx/main/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'main/register.html', {'form': form})
 
 @login_required
 def home(request):
